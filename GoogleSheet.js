acbff7483708b2a61a1b0b2dc5048f45ff10f83a
The Mindless Robot <joelarias@protonmail.com>
2020-05-01 12:51:14 -0700

    fixed callback return value

--- GoogleSheet.js
+++ GoogleSheet.js
@@ -1,5 +1,3 @@
-// 19h9Wsj01KZbQmdWC4QhJ-zRTOlvyFHRwFQmAJWF1F4Q
-
 class GoogleSheet {
     constructor(id, sheetNumber = 1) {
         this.id = id;
@@ -33,60 +31,11 @@ class GoogleSheet {
         sheet[sheetName] = rows;
 
         if(callback && typeof callback == 'function') {
-            callback(sheet);
-            return;
+           return callback(sheet);
+
         }
         return sheet;
     }
 }
 
 
-
-// function init(sheetNumber) {
-// 	console.log('app loaded');
-// 	loadJSON(sheetNumber);
-// }
-// function format(value) {
-// 	return value;
-// }
-// function loadJSON(sheetNumber) {
-// 	const id = '1mSjbAORDq8HxoWT10a220onnIJYiVEGNK5j2PZnEVYE';
-// 	const sheetNum = Number(sheetNumber);
-// 	console.log('SHEET:', sheetNum);
-// 	const url = 'https://spreadsheets.google.com/feeds/list/' + id + '/' + sheetNum + '/public/values?alt=json';
-
-// 	new Promise(function(resolve, reject){
-// 		return fetch(url)
-// 			.then(response => response.json())
-// 			.then(data => {
-// 			// console.log('data =>', data);
-// 			let sheet = {};
-// 			let sheetName = data.feed.title.$t;
-// 			let rows = [];
-
-// 			if(data.feed.entry) {
-// 				data.feed.entry.forEach(row => {
-// 					// console.log('row => ', row);
-// 					let rowObj = {};
-// 					for (let field in row) {
-// 						if (field.substring(0, 3) == 'gsx') {
-// 							rowObj[field.split('$')[1]] = format(row[field].$t);
-// 						}
-// 					}
-// 					rows.push(rowObj);
-// 				});
-// 			} else {
-// 				let rowObj = {};
-// 				rowObj.subject = 'TBD';
-// 				rows.push(rowObj);
-// 			}
-
-// 			console.log('cleanRows =>', rows);
-// 			sheet[sheetName] = rows;
-// 			resolve(sheet);
-// 		})
-// 	}).then(sheet => {
-// 		console.log('sheet =>', sheet);
-// 		//loadOutput(sheet);
-// 	})
-// }
