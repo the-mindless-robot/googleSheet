d20efc93d1e10e880c891fee3dffbc08d7780458
The Mindless Robot <joelarias@protonmail.com>
2021-08-18 16:04:19 -0700

    handles multiple sheets

--- GoogleSheet.js
+++ GoogleSheet.js
@@ -3,39 +3,48 @@ class GoogleSheet {
 		this.id = id;
 	}
 
-	async load(callback = false, options = false) {
+	async load(callback = false) {
 		const sheet = {};
-		let moreData;
-		let sheetNumber = options && options.hasOwnProperty('start') ? options.start : 1;
-		const sheetNumberEnd = options && options.hasOwnProperty('end') ? options.end : false;
-		const APIkey = 'xxxxx';
 
-		do {
-			moreData = false;
-			const v4 = 'https://sheets.googleapis.com/v4/spreadsheets/' + this.id + '/values/Sheet' + sheetNumber + '?key='+ APIkey;
+		const APIkey = config.apiKey;
+		const sheetTitles = await this._getSheetTitles(APIkey);
+		console.debug('titles', sheetTitles);
 
-			const response = await fetch(v4);
-			// console.log('response', response);
-
-			const data = response.status == 200 ? await response.json() : false;
-
+		for(const title of sheetTitles) {
+			const url = 'https://sheets.googleapis.com/v4/spreadsheets/' + this.id + '/values/' + title + '?key='+ APIkey;
+			const data = await this._requestData(url);
 			if(data) {
-
 				const rows = this._buildRowsV4(data);
-				sheet[`sheet${sheetNumber}`] = rows;
-
-				if(!sheetNumberEnd || sheetNumber != sheetNumberEnd) {
-					sheetNumber++;
-					moreData = true;
-				}
+				sheet[title] = rows;
 			}
 		}
-		while (moreData);
 
 		if(callback && typeof callback == 'function') {
 			return callback(sheet);
 		}
 		return sheet;
+	}
+
+	async _requestData(url) {
+		const response = await fetch(url);
+		const data = response.status == 200 ? await response.json() : false;
+		return data;
+	}
+
+	async _getSheetTitles(APIkey) {
+		const sheets = [];
+		const url = 'https://sheets.googleapis.com/v4/spreadsheets/' + this.id + '/?key='+ APIkey;
+		const data = await this._requestData(url);
+
+		if(data) {
+			for(const sheet of data.sheets) {
+				sheets.push(sheet.properties.title);
+			}
+			return sheets;
+		}
+
+		return false;
+
 	}
 
 	_buildRowsV4(data) {
@@ -52,7 +61,7 @@ class GoogleSheet {
 
 				for(let i=0; i < labels.length; i++) {
 					const label = this._removeSpaces(labels[i].trim().toLowerCase());
-					const value = rowValues[i].trim() ?? '';
+					const value = rowValues[i]?.trim() ?? '';
 
 					//                 console.debug(`${label} : ${value}`);
 					rowObj[label] = value;
